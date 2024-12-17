cefe10b250c08f4beefeb2e955a5ee34740ea4f3
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2024-12-17 11:36:44 +0300

    mail service done

---
+++ src/main/java/perso/utilisateur/services/MailService.java
@@ -0,0 +1,34 @@
+package perso.utilisateur.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import perso.utilisateur.models.Utilisateur;
+import perso.utilisateur.repositories.UtilisateurRepo;
+
+@Service
+public class MailService {
+	@Autowired
+	private JavaMailSender mailSender;
+	@Autowired
+	private UtilisateurRepo utilisateurRepo;
+
+	public void sendEmail(String to, String subject, String body) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(body);
+		message.setFrom("your-email@gmail.com");
+
+		mailSender.send(message);
+	}
+
+	public void sendEmail(Object utilisateur) {
+		if (utilisateur instanceof Integer) {
+			utilisateur = utilisateurRepo.findById((Integer) utilisateur).get();
+		}
+		sendEmail(((Utilisateur)utilisateur).getEmail(),"Pin code", ((Utilisateur)utilisateur).getPin().getPinValue());
+	}
+}
--- src/test/java/perso/utilisateur/UtilisateurApplicationTests.java
+++ src/test/java/perso/utilisateur/UtilisateurApplicationTests.java
@@ -1,13 +1,20 @@
 package perso.utilisateur;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import perso.utilisateur.services.MailService;
 
 @SpringBootTest
 class UtilisateurApplicationTests {
 
+	@Autowired
+	MailService mailService;
+
 	@Test
 	void contextLoads() {
 	}
+
 
 }
